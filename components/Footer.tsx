2783acf7b079c63e7cb368db01f503fc7b5a466b
nenshallom <sshallom543@gmail.com>
2026-01-14 00:54:23 +0100

    Fix: light theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,17 +7,17 @@ export default function Footer() {
   return (
     // REMOVED "fixed bottom-0 left-0 right-0"
     // ADDED "w-full"
-    <footer className="w-full z-40 bg-background/95 backdrop-blur-md pb-6 pt-2 px-4 border-t border-dashed border-primary/30">
+    <footer className="w-full z-40 bg-background/95 backdrop-blur-md pb-6 pt-2 px-4">
       
       {/* 1. Navigation Tabs */}
       <div className="flex justify-between gap-2 mb-2 text-xs font-mono">
-        <Link href="/projects" className="flex-1 text-center py-2 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
+        <Link href="/projects" className="flex-1 text-center py-1 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
           Projects
         </Link>
-        <Link href="/experience" className="flex-1 text-center py-2 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
+        <Link href="/experience" className="flex-1 text-center py-1 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
           Experience    
         </Link>
-        <Link href="/tools" className="flex-1 text-center py-2 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
+        <Link href="/tools" className="flex-1 text-center py-1 border border-dashed border-primary text-primary hover:bg-primary/10 transition rounded">
           Tools
         </Link>
       </div>
@@ -36,10 +36,10 @@ export default function Footer() {
       <div className="flex items-center gap-3">
         {/* Socials */}
         <div className="flex gap-2">
-          <a href="https://github.com" target="_blank" className="p-2 border border-dashed border-primary/50 rounded text-white hover:text-primary hover:border-primary transition">
+          <a href="https://github.com/nenshallom" target="_blank" className="p-2 border border-dashed border-primary/50 rounded text-white dark:text-black hover:text-primary hover:border-primary transition">
             <Github size={15} />
           </a>
-          <a href="https://linkedin.com" target="_blank" className="p-2 border border-dashed border-primary/50 rounded text-white hover:text-primary hover:border-primary transition">
+          <a href="https://linkedin.com/in/nenshallom/" target="_blank" className="p-2 border border-dashed border-primary/50 rounded text-white dark:text-black hover:text-primary hover:border-primary transition">
             <Linkedin size={15} />
           </a>
         </div>
@@ -51,10 +51,10 @@ export default function Footer() {
 
         {/* Contact */}
         <div className="flex gap-2">
-          <a href="mailto:your@email.com" className="p-2 border border-dashed border-primary/50 rounded text-white hover:text-primary hover:border-primary transition">
+          <a href="mailto:sshallom92@email.com" className="p-2 border border-dashed border-primary/50 rounded text-white dark:text-black hover:text-primary hover:border-primary transition">
             <Mail size={15} />
           </a>
-          <a href="tel:+123456789" className="p-2 border border-dashed border-primary/50 rounded text-white hover:text-primary hover:border-primary transition">
+          <a href="tel:+2349065679171" className="p-2 border border-dashed border-primary/50 rounded text-white dark:text-black hover:text-primary hover:border-primary transition">
             <Phone size={15} />
           </a>
         </div>
--- components/home/SlideOne.tsx
+++ components/home/SlideOne.tsx
@@ -5,7 +5,7 @@ export default function SlideOne() {
   return (
     <motion.div className="flex flex-col items-center relative h-full justify-center">
       
-      <div className="w-full flex flex-col gap-2 mb-4 relative z-10">
+      <div className="w-full flex flex-col gap-1 mb-4 relative z-10">
         <p className="text-accent font-bold tracking-wide text-left self-start text-sm">
           Welcome, I&apos;m
         </p>
@@ -18,7 +18,7 @@ export default function SlideOne() {
             </h1>
 
             {/* --- PROFILE IMAGE SECTION --- */}
-            <div className="w-40 h-40 md:w-40 md:h-40 rounded-full border-[3px] border-gray-800 overflow-hidden shrink-0  bg-gray-900 relative z-20 shadow-xl">
+            <div className="w-40 h-40 md:w-40 md:h-40 rounded-full border-[2px] border-gray-800 overflow-hidden shrink-0  bg-gray-900 relative z-20 shadow-xl">
                 {/* Next.js Image Component:
                    - src: starts from 'public' folder (so /images/...)
                    - width/height: required for aspect ratio (matches w-40 = 160px)
@@ -45,7 +45,7 @@ export default function SlideOne() {
         </span>
       </div>
 
-      <h2 className="self-start text-lg md:text-2xl font-bold text-black dark:text-white leading-snug font-mono relative z-10">
+      <h2 className="self-start text-2xl md:text-3xl font-bold text-black dark:text-white leading-snug font-mono relative z-10">
         Engineering Clean, <br />
         Beautiful & Performant <br />
         Frontend for the Web
